b8e7db8dffe31e03325a9d42a2c720aad80985df
KickEGG <449357528@QQ.COM>
2017-04-04 13:02:18 +0800

    2017-4-4 测试提交

---
+++ src/main/java/com/kickegg/time/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.kickegg.time;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicReference;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        AtomicReference<String> currentTime;
+        currentTime = new AtomicReference<String>();
+        currentTime.set(dateFormat.format(new Date()));
+        req.setAttribute("currentTime", currentTime.get());
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+        System.out.print("test git push");
+        System.out.print("test git push");
+    }
+}
