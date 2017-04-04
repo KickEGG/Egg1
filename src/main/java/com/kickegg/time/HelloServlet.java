3a25ea95923c00d3670f0f2d3fb3beaafe680543
KickEGG <449357528@QQ.COM>
2017-04-04 17:22:15 +0800

    2017-4-4 测试提交2

--- src/main/java/com/kickegg/time/HelloServlet.java
+++ src/main/java/com/kickegg/time/HelloServlet.java
@@ -21,5 +21,6 @@ public class HelloServlet extends HttpServlet {
         currentTime = new AtomicReference<String>();
         currentTime.set(dateFormat.format(new Date()));
         req.setAttribute("currentTime", currentTime.get());
+        //测试github
     }
 }
