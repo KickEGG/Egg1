1101057037f14883e87f5c79c6970003a69ec841
ck <chenkai@x9v.com.cn>
2017-04-16 22:50:16 +0800

    2017-4-16 customservice modified

--- src/main/java/com/kickegg/busi/service/CustomerService.java
+++ src/main/java/com/kickegg/busi/service/CustomerService.java
@@ -2,22 +2,80 @@ package com.kickegg.busi.service;
 
 import com.kickegg.busi.helper.DatabaseHelper;
 import com.kickegg.busi.model.Customer;
+import com.kickegg.busi.util.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 
 /**
  * 提供客户数据服务
  */
 public class CustomerService {
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc:driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("can not load jdbc driver", e);
+        }
+    }
 
     /**
      * 获取客户列表
      */
-    public List<Customer> getCustomerList() {
-        String sql = "SELECT * FROM customer";
-        return DatabaseHelper.queryEntityList(Customer.class, sql);
+//    public List<Customer> getCustomerList() {
+//        String sql = "SELECT * FROM customer";
+//        return DatabaseHelper.queryEntityList(Customer.class, sql);
+//    }
+    public List<Customer> getCustomerList(String keyword) {
+        //to do something
+        Connection conn = null;
+        try {
+            List<Customer> customerList = new ArrayList<Customer>();
+            String sql = "select * from customer";
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getLong("id"));
+                customer.setName(rs.getString("name"));
+                customer.setContact(rs.getString("contact"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setEmail(rs.getString("email"));
+                customer.setRemark(rs.getString("remark"));
+                customerList.add(customer);
+            }
+            return customerList;
+        } catch (SQLException e) {
+            LOGGER.error("execute sql failure", e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    LOGGER.error("close connection failure", e);
+                }
+            }
+        }
+        return null;
     }
 
     /**
